9cc51c761f9bc7f7a728d49a6dd9dd72815133f2
Ash <ash@flarpyland.com>
2026-02-08 11:40:19 +0000

    Moved the results caching into the query function, it shouldn't be in a view

--- search/util.py
+++ search/util.py
@@ -1,11 +1,16 @@
 from api.models.arches import ArchesInstance, Concept
-import re, asyncio
+from django.core.cache import cache
+import re, asyncio, hashlib
 
 def strip_html_tags(text):
     return (re.sub(r'<.*?>', ' ', text)).strip()
 
 def keyword_search(query_string):
     global results, ct, loop
+    cache_key = hashlib.sha1(query_string.encode('utf8')).hexdigest()
+    results = cache.get(cache_key)
+    if not results is None:
+        return results
     results = []
     ct = ArchesInstance.objects.count()
     loop = asyncio.new_event_loop()
@@ -30,6 +35,7 @@ def keyword_search(query_string):
         if 'displaydescription' in x['_source']:
             x['_source']['displaydescription'] = strip_html_tags(x['_source']['displaydescription'])
         ret.append(x['_source'])
+    cache.set(cache_key, ret, 300) # Cache the results for five minutes
     return ret
 
 def concept_search(query_string):
--- search/views.py
+++ search/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
 from search.util import keyword_search, concept_search
 
@@ -12,9 +11,6 @@ def results(request):
 	mode = 'list'
 	if 'category_map' in request.POST:
 		mode = 'map'
-	results = cache.get(query)
-	if results is None:
-		results = keyword_search(query)
-		cache.set(query, results, 300) # Cache the results for five minutes
+	results = keyword_search(query)
 	concepts = concept_search(query)
 	return render(request, 'search/results.html', {"query": query, "mode": mode, "results": results, "concepts": concepts})
